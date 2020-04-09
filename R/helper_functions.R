acece7308530d13a48996c4c701fb3b1e7b0c288
Alexander Piper <alexpiperdesigns@gmail.com>
2020-04-09 13:36:51 +1000

    Added primer statistics function

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,4 +1,66 @@
 
+# Get_primer_metrics ------------------------------------------------------
+
+get_primer_statistics <- function(x, metrics = "all", disambiguate = TRUE){
+  #Get metrics
+  availmetrics <- c("base_freq","clamps",
+                    "tm", "homopolymer",
+                    "degeneracy", "primer_length")
+  if (length (metrics)== 1 && metrics =="all"){
+    metrics <- availmetrics
+  }
+  if(any(!metrics %in% availmetrics )){
+    stop("Error, invalid metric: ", metrics[!metrics %in% availmetrics])
+  }
+  
+  # Replace inosines
+  x <- x %>% stringr::str_replace_all("I", "N")
+  if(disambiguate){
+    query <- unlist(DECIPHER::Disambiguate(DNAStringSet(x)))
+  } else {
+    query <- DNAString(x)
+  }
+  if("base_freq" %in% metrics){
+    base_freq <- letterFrequency(query,letters="ACGT", OR=0) %>%
+      prop.table() %>%
+      colSums() %>%
+      as.matrix() %>%
+      t() %>%
+      as.data.frame() %>%
+      dplyr::mutate(`GC%` = (G+C))
+  }
+  if ("clamps" %in% metrics){ # Should ambiguous bases count?
+    clamps <- data.frame(
+      GC_last2 = x %>% str_extract("..$") %>% str_count("G|C"),
+      GC_last5 = x %>% str_extract(".....$") %>% str_count("G|C")
+    )
+  }
+  if ("tm" %in% metrics){
+    tm <- data.frame(tm= TmCalculator::Tm_NN(x, ambiguous = disambiguate))
+  }  
+  if("homopolymer" %in% metrics){
+    homopolymer <- data.frame(
+      `poly_A` = query %>% as.character() %>% purrr::map_chr(longestConsecutive, "A") %>% max(),
+      `poly_T` = query %>% as.character() %>%purrr::map_chr(longestConsecutive, "T") %>% max(),
+      `poly_G` = query %>% as.character() %>% purrr::map_chr(longestConsecutive, "G") %>% max(),
+      `poly_C` = query %>% as.character() %>% purrr::map_chr(longestConsecutive, "C") %>% max(),
+      stringsAsFactors = FALSE
+    )
+  }
+  if("degeneracy" %in% metrics){
+    degeneracy <- data.frame(degeneracy = query %>% DECIPHER::Disambiguate() %>% length())
+  } 
+  if("primer_length" %in% metrics){
+    primer_length <- data.frame(length = x %>% nchar())
+  } 
+  
+  out <- metrics %>% 
+    purrr::map(get, envir=sys.frame(sys.parent(0))) %>%
+    bind_cols() %>%
+    mutate(seq = x)
+  return(out)
+}
+
 # Parralel slidenucdiag ---------------------------------------------------
 
 slideNucDiag_para <- function (DNAbin, sppVector, width, interval = 1, cores=1) {
